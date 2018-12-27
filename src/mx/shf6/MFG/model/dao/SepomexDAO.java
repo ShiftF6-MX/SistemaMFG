54623f9d54d6a2b723135ebbafae4a691df8c1fa
Joel Grande <joel.grande@gruposhift-f6.com.mx>
2018-12-27 08:57:31 -0600

    Se agrego la clase <DAO> SepomexDAO al proyecto

--- src/mx/shf6/MFG/model/dao/SepomexDAO.java
+++ src/mx/shf6/MFG/model/dao/SepomexDAO.java
@@ -7,6 +7,7 @@ import java.sql.Statement;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import mx.shf6.MFG.utilities.Notificacion;
 
 public class SepomexDAO {
 
@@ -18,36 +19,31 @@ public class SepomexDAO {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
 			while (resultSet.next()) {
-				listaEstados.add(resultSet.getString(1));
-				
-			}
+				listaEstados.add(resultSet.getString(1));				
+			}//FIN WHILE
 		}catch (SQLException e) {
-			System.out.println("Error: En método leer");
-			e.printStackTrace();
-		}
+			Notificacion.dialogoException(e);
+		}//FIN TRY-CATCH
 		return listaEstados;
-	}
+	}//FIN METODO
 	
 	public ObservableList<String> leerMunicipios(Connection connection, String estado) {
 		String query ="";
 		ObservableList<String> listaEstados = FXCollections.observableArrayList();
 		query="SELECT municipio "
 				+ "FROM sepomex "
-				+ "WHERE Estado='"+estado+"' "
+				+ "WHERE Estado='" + estado + "' "
 				+ "GROUP BY municipio "
 				+ "ORDER BY municipio ASC;";
 		try {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
 			while (resultSet.next()) {
-				listaEstados.add(resultSet.getString(1));
-				
-			}
+				listaEstados.add(resultSet.getString(1));				
+			}//FIN WHILE
 		}catch (SQLException e) {
-			System.out.println("Error: En método leer");
-			e.printStackTrace();
-		}
+			Notificacion.dialogoException(e);
+		}//FIN TRY-CATCH
 		return listaEstados;
-	}
-
-}
+	}//FIN METODO
+}//FIN CLASE
